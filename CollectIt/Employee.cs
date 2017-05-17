a3d01f516bd0fbfa62a791b7083eebdb2dcaf4e0
Daniels <Daniels.James@mayo.edu>
2017-05-17 17:28:59 -0500

    Initial commit after Generics m2c2

---
+++ CollectIt/Employee.cs
@@ -0,0 +1,8 @@
+﻿namespace CollectIt
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int DepartmentId { get; set; }
+    }
+}
---
+++ CollectIt/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<Employee> line = new Queue<Employee>();
+            // with a QQueue there is no add nor insert method, so use Enqueue
+            line.Enqueue(new Employee { Name = "Alex " });
+            line.Enqueue(new Employee { Name = "Dani" });
+            line.Enqueue(new Employee { Name = "Johnson" });
+
+            // the whole point is you get things out as you put them in so no accessing by:
+            // line[0]... etc.
+
+            // Dequeue gives you the first item in teh line and deletes it from queue at teh same time
+            // line.Dequeue ...
+
+            // to get everything, use a loop
+            while(line.Count > 0)
+            {
+                var employee = line.Dequeue();
+                Console.WriteLine(employee.Name); // will spit out all items in same order entered. FIFO
+            }
+        }
+    }
+}
