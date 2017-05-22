77fb0b88fd494796db98856fbd72e067f230632e
Daniels <Daniels.James@mayo.edu>
2017-05-22 16:58:35 -0500

    Makes more complex dictionary, adds unit tests. m2c6

---
+++ CollectIt.Tests/UnitTest2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace CollectIt.Tests
+{
+    [TestClass]
+    public class DictionaryTests
+    {
+        [TestMethod]
+        public void Can_Use_Dictionary_As_Map()
+        {
+            var map = new Dictionary<int, string>();
+            map.Add(1, "one");
+            map.Add(2, "two");
+
+            Assert.AreEqual("one", map[1]);
+        }
+
+        [TestMethod]
+        public void Can_Search_Key_With_ContainsKey()
+        {
+            var map = new Dictionary<int, string>();
+            map.Add(1, "one");
+            map.Add(2, "two");
+
+            Assert.IsTrue(map.ContainsKey(2));
+        }
+
+        [TestMethod]
+        public void Can_Remove_By_Key()
+        {
+            var map = new Dictionary<int, string>();
+            map.Add(1, "one");
+            map.Add(2, "two");
+            Assert.IsTrue(map.ContainsKey(2));
+
+            map.Remove(2);
+
+            Assert.IsFalse(map.ContainsKey(2));
+        }
+    }
+}
--- CollectIt/Program.cs
+++ CollectIt/Program.cs
@@ -13,8 +13,10 @@ namespace CollectIt
             // Dictionary data structure has a key and value. Makes finding data faster.
             //Dictionary<string, Employee> employeesByName = new Dictionary<string, Employee>();
             // Scott likes the var kw in these situations for readability.
+            /*
             var employeesByName = new Dictionary<string, Employee>();
 
+            // key values must be unique. you'll get an error if you try to add another with teh same key.
             employeesByName.Add("Scott", new Employee { Name = "Scott Bromander" });
             employeesByName.Add("Joul", new Employee { Name = "Joul Webb" });
             employeesByName.Add("Harry", new Employee { Name = "Harry Flandersen" });
@@ -25,6 +27,25 @@ namespace CollectIt
             {
                 Console.WriteLine("{0}:{1}", item.Key, item.Value.Name);
             }
+            */
+
+            // here, making it so that each item is actually a list of its own. This solves teh problem of what happens wehn there are two "Scotts".
+            var employeesByName = new Dictionary<string, List<Employee>>();
+
+            employeesByName.Add("Scott", new List<Employee>() { new Employee { Name = "Scoot Allen" } });
+
+            // ... other employees are added.
+
+            // need to add another Scott, so instead of adding new, call up the existing key scott, and add to its List
+            employeesByName["Scott"].Add(new Employee { Name = "Scott Bromander" });
+
+            foreach (var item in employeesByName)
+            {
+                foreach (var employee in item.Value)
+                {
+                    Console.WriteLine(employee.Name);
+                }
+            }
         }
-    }
+    } 
 }
