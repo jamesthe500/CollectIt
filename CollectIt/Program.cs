21d17b9ecf02ab7be80394e4178d879d3d59526d
Daniels <Daniels.James@mayo.edu>
2017-05-29 09:15:36 -0500

    Sorts departments. Prevents duplicate employees. m3c7

--- CollectIt/Program.cs
+++ CollectIt/Program.cs
@@ -6,26 +6,53 @@ using System.Threading.Tasks;
 
 namespace CollectIt
 {
+    // This class is a tool for HashSets to compare emploees. Tells what to compare.
+    // Without it, the HashSets are comparing the entire object not knowing that it's the Name we are looking at.
+    public class EmployeeComparer : IEqualityComparer<Employee>
+    {
+        public bool Equals(Employee x, Employee y)
+        {
+            return String.Equals(x.Name, y.Name);
+        }
+
+        public int GetHashCode(Employee obj)
+        {
+            return obj.Name.GetHashCode();
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            // The dictionary we made last mod isn't good at sorting.
+            // Now to comapre employees
 
-            // SortedDictionary is an option. It automatically stores in ascending order Wors well for dates, strings ints etc.
-            // there are ways to tell it how to sort, not getting into it here.
-            var employeesByName = new SortedDictionary<string, List<Employee>>();
+            // made as a Dictionary at first, changed to SortedDictionary so it will be always sorted.
+            // Changed from list to HashSet so as to prevent duplicates.
+            var departments = new SortedDictionary<string, HashSet<Employee>>();
 
-            // two lists for HR, one with two other with three
-            employeesByName.Add("Sales", new List<Employee> { new Employee(), new Employee(), new Employee() });
-            employeesByName.Add("Engineering", new List<Employee> { new Employee(), new Employee() });
+            departments.Add("Sales", new HashSet<Employee>(new EmployeeComparer()));
+            departments["Sales"].Add(new Employee { Name = "Florence" });
+            departments["Sales"].Add(new Employee { Name = "Mr. Roboto" });
+            departments["Sales"].Add(new Employee { Name = "Gerry" });
+            departments["Sales"].Add(new Employee { Name = "Mandi" });
+            // we don't want there to be duplicates. 
+            // Strainght-up HashSet won't work since these are objects of employee. 
+            // C# doesn't know how to know that these are the same
+            departments["Sales"].Add(new Employee { Name = "Mandi" });
 
-            foreach (var item in employeesByName)
+            departments.Add("Engineering", new HashSet<Employee>(new EmployeeComparer()));
+            departments["Engineering"].Add(new Employee { Name = "Scott" });
+            departments["Engineering"].Add(new Employee { Name = "Joe" });
+            departments["Engineering"].Add(new Employee { Name = "Dianne" });
+
+            foreach (var department in departments)
             {
-                Console.WriteLine("the count of employees in {0} is {1}", 
-                    item.Key, item.Value.Count 
-                    );
-                //Console.WriteLine("hello");
+                Console.WriteLine(department.Key);
+                foreach (var employee in department.Value)
+                {
+                    Console.WriteLine("\t" + employee.Name);
+                }
             }
 
 
